df0c5a8288d392ac7f1b37e25b86dca9ee423c4b
aigen31 <aigen.chernogorsk@yandex.ru>
2020-04-29 16:31:32 +0700

    Remove

--- js/common.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	jQuery(document).ready(function() {
-        jQuery('.post').addClass("hidden").viewportChecker({
-        classToAdd: 'visible animated slow slideInUp',
-        offset: 100
-        });
-        });
-});
