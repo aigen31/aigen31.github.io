59f29a38de21f901c63a0acf7929cb8da219ee0d
aigen31 <aigen.chernogorsk@yandex.ru>
2020-05-03 17:12:21 +0700

    Added Burger Animation

--- blake-cheek/js/common.js
+++ blake-cheek/js/common.js
@@ -6,3 +6,9 @@ $(function() {
         });
         });
 });
+$(function(){
+        document.querySelector(".menu-btn").onchange = function(){
+                document.querySelector(".navigation").classList.toggle("checked", this.checked);
+                document.querySelector("body").classList.toggle("menu", this.checked);
+        }
+});
