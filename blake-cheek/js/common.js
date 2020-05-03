9938f8667b806c41459171d1f4339d87a9a5bd49
aigen31 <aigen.chernogorsk@yandex.ru>
2020-05-03 17:25:16 +0700

    Fixed Menu Icon

--- blake-cheek/js/common.js
+++ blake-cheek/js/common.js
@@ -2,7 +2,7 @@ $(function() {
 	jQuery(document).ready(function() {
         jQuery('.post').addClass("hidden").viewportChecker({
         classToAdd: 'visible animated slow slideInUp',
-        offset: 100
+        offset: 0
         });
         });
 });
