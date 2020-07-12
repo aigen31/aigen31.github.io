cb676165e189a283b390fe7b1f11aaf3cfae5092
AiGen31 <aigen.chernogorsk@yandex.ru>
2020-07-12 18:49:39 +0700

    Adding Site «Expoforum»

---
+++ expoforum/js/common.js
@@ -0,0 +1,22 @@
+$(function(){
+	$("a[href^='#']").click(function(){
+			var _href = $(this).attr("href");
+			$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+			return false;
+	});
+
+	$("#my-menu").mmenu({
+		extensions: ['position-right'],
+		navbar: {
+			title: '<img src="img/expoforum-logo1.svg" alt="Бьюти - супер кресло">'
+		}
+	});
+
+	var api = $('#my-menu').data('mmenu');
+	api.bind('open:finish', function(){
+		$('.hamburger').addClass('is-active');
+	});
+	api.bind('close:finish', function(){
+		$('.hamburger').removeClass('is-active');
+	})
+});
